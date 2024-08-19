10676f771f453c4f6ff7231222cc1f79ba18e713
hi-lee-mon <shunmel.09@gmail.com>
2024-08-19 09:27:36 +0900

    ✨ feat(playground): h1の追加
    h1とページを説明する文言を追加した

--- app/(default)/playground/page.tsx
+++ app/(default)/playground/page.tsx
@@ -72,19 +72,25 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div className="flex flex-wrap gap-2">
-      {contents.map((content) => (
-        <div key={content.path}>
-          <div className="flex gap-4">
-            <Button variant="outline" asChild>
-              <Link href={`/playground/${content.path}`} className="flex gap-2">
-                {Types[content.type]}
-                {content.title}
-              </Link>
-            </Button>
+    <div>
+      <div className="mb-10 flex flex-col gap-4">
+        <h1 className="text-balance text-4xl font-bold">Shunの遊び場</h1>
+        <p className="text-muted-foreground">Shunが実装できるものを一覧化したページです。</p>
+      </div>
+      <div className="flex flex-wrap gap-2">
+        {contents.map((content) => (
+          <div key={content.path}>
+            <div className="flex gap-4">
+              <Button variant="outline" asChild>
+                <Link href={`/playground/${content.path}`} className="flex gap-2">
+                  {Types[content.type]}
+                  {content.title}
+                </Link>
+              </Button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
