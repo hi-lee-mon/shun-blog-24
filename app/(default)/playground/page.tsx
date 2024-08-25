b63617c575d9f7d511d2b28feec4a3bf27377e2c
hi-lee-mon <shunmel.09@gmail.com>
2024-08-26 02:33:47 +0900

    🐞 fix(playground): buildエラー修正
    page.tsxにてexportしていた定数をconstantsフォルダに移動

--- app/(default)/playground/page.tsx
+++ app/(default)/playground/page.tsx
@@ -1,69 +1,7 @@
-import { MdxLogo } from '@/components/logo/mdx-logo'
-import { NextjsLogo } from '@/components/logo/nextjs-logo'
-import { ReactHookFormLogo } from '@/components/logo/react-hook-form-logo'
-import { ReactLogo } from '@/components/logo/react-logo'
 import { Button } from '@/components/ui/button'
-import { Wrench } from 'lucide-react'
+import { contentsType, playgroundContents } from '@/constants/playground'
 import type { Metadata } from 'next'
 import Link from 'next/link'
-
-const Types = {
-  none: null,
-  next: <NextjsLogo />,
-  wip: <Wrench />,
-  react: <ReactLogo />,
-  reactHookFrom: <ReactHookFormLogo />,
-  mdx: <MdxLogo />,
-}
-
-type Content = {
-  title: string
-  path: string
-  type: keyof typeof Types
-}
-
-export const playgroundContents: Content[] = [
-  {
-    title: '実験用',
-    path: 'wip',
-    type: 'none',
-  },
-  {
-    title: 'リンクカード',
-    path: 'link-card',
-    type: 'none',
-  },
-  {
-    title: 'ヘッダーメニュー',
-    path: 'header-menu',
-    type: 'none',
-  },
-  {
-    title: 'カード',
-    path: 'card',
-    type: 'none',
-  },
-  {
-    title: 'パラレルルート・インターセプトルート',
-    path: 'para-inter-root',
-    type: 'next',
-  },
-  {
-    title: 'forwardRef',
-    path: 'forward-ref',
-    type: 'react',
-  },
-  {
-    title: '送信確認画面',
-    path: 'react-hook-form-confirm',
-    type: 'reactHookFrom',
-  },
-  {
-    title: 'MDX',
-    path: 'mdx',
-    type: 'mdx',
-  },
-]
 
 export const metadata: Metadata = {
   title: 'Playground',
@@ -83,7 +21,7 @@ export default function Page() {
             <div className="flex gap-4">
               <Button variant="outline" asChild>
                 <Link href={`/playground/${content.path}`} className="flex gap-2">
-                  {Types[content.type]}
+                  {contentsType[content.type]}
                   {content.title}
                 </Link>
               </Button>
---
+++ constants/playground.tsx
@@ -0,0 +1,63 @@
+import { MdxLogo } from '@/components/logo/mdx-logo'
+import { NextjsLogo } from '@/components/logo/nextjs-logo'
+import { ReactHookFormLogo } from '@/components/logo/react-hook-form-logo'
+import { ReactLogo } from '@/components/logo/react-logo'
+import { Wrench } from 'lucide-react'
+
+export const contentsType = {
+  none: null,
+  next: <NextjsLogo />,
+  wip: <Wrench />,
+  react: <ReactLogo />,
+  reactHookFrom: <ReactHookFormLogo />,
+  mdx: <MdxLogo />,
+}
+
+type Content = {
+  title: string
+  path: string
+  type: keyof typeof contentsType
+}
+
+export const playgroundContents: Content[] = [
+  {
+    title: '実験用',
+    path: 'wip',
+    type: 'none',
+  },
+  {
+    title: 'リンクカード',
+    path: 'link-card',
+    type: 'none',
+  },
+  {
+    title: 'ヘッダーメニュー',
+    path: 'header-menu',
+    type: 'none',
+  },
+  {
+    title: 'カード',
+    path: 'card',
+    type: 'none',
+  },
+  {
+    title: 'パラレルルート・インターセプトルート',
+    path: 'para-inter-root',
+    type: 'next',
+  },
+  {
+    title: 'forwardRef',
+    path: 'forward-ref',
+    type: 'react',
+  },
+  {
+    title: '送信確認画面',
+    path: 'react-hook-form-confirm',
+    type: 'reactHookFrom',
+  },
+  {
+    title: 'MDX',
+    path: 'mdx',
+    type: 'mdx',
+  },
+]
