3170a51813e10a3153ae224cea0b6f8b97cce448
hi-lee-mon <shunmel.09@gmail.com>
2024-08-26 20:53:49 +0900

    🦄 refactor(blog): 未使用コンポーネントの削除

--- app/(default)/blog/_components/mdx-layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function MdxLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="prose mx-auto dark:prose-invert  [&_a:hover]:underline [&_a]:no-underline">
-      {children}
-    </div>
-  )
-}
