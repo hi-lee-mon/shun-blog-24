7fed805efb1d9ae012a098fb3a0b15e1c3394b0c
hi-lee-mon <shunmel.09@gmail.com>
2024-08-28 08:05:44 +0900

    🌈 style(global): fix:prettierの実行

--- app/(default)/_components/section.tsx
+++ app/(default)/_components/section.tsx
@@ -9,7 +9,7 @@ export const Section = ({
 }) => {
   return (
     <section className={className}>
-      <h2 className="inline-block	 border-b-2 text-xl font-bold">{title}</h2>
+      <h2 className="inline-block border-b-2 text-xl font-bold">{title}</h2>
       <div className="p-4">{children}</div>
     </section>
   )
--- app/(default)/playground/mdx/mdx-layout.tsx
+++ app/(default)/playground/mdx/mdx-layout.tsx
@@ -1,7 +1,7 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
   return (
-    <div className="prose mx-auto dark:prose-invert  [&_a:hover]:underline [&_a]:no-underline">
+    <div className="prose mx-auto dark:prose-invert [&_a:hover]:underline [&_a]:no-underline">
       {children}
     </div>
   )
