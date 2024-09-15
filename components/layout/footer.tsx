f8d1d8b372b8a6dbe1fa9ca5f66bc0327cb55342
hi-lee-mon <shunmel.09@gmail.com>
2024-09-16 01:37:05 +0900

    ✨ feat(footer): footerにgithubリンクを追加

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,9 +1,16 @@
+import { GithubLogo } from '@/components/logo/github-logo'
+import { myWebs } from '@/constants/myWeb'
+
 export const Footer = () => {
+  const github = myWebs.find((myWeb) => myWeb.linkText === 'GitHub')
   return (
     //  footerを画面下部に配置する場合、親要素のクラスに依存する。左記のクラスを親に設定すること。className="min-h-dvh flex flex-col"
     <footer className="sticky top-full border-t">
       <div className="container flex h-16 items-center">
         <p className="flex-1 text-muted-foreground">&copy; shun 2024</p>
+        <a href={github?.href} target="_blank" rel="noreferrer" aria-label={github?.linkText}>
+          <GithubLogo ariaHidden />
+        </a>
       </div>
     </footer>
   )
---
+++ constants/myWeb.tsx
@@ -0,0 +1,20 @@
+import { GithubLogo } from '@/components/logo/github-logo'
+import { ZennLogo } from '@/components/logo/zenn-logo'
+
+export const myWebs = [
+  {
+    logo: <ZennLogo ariaHidden />,
+    href: 'https://zenn.dev/hokoripon',
+    linkText: 'Zenn',
+  },
+  {
+    logo: <GithubLogo ariaHidden />,
+    href: 'https://github.com/hi-lee-mon/shun-blog-24',
+    linkText: 'GitHub',
+  },
+  {
+    logo: <ZennLogo ariaHidden />,
+    href: 'https://zenn.dev/nash',
+    linkText: '現在は更新していないZenn',
+  },
+] as const
