0a35d5b37f61f0bf72b15084d26a3ed5b6c5580d
hi-lee-mon <shunmel.09@gmail.com>
2024-09-16 00:51:35 +0900

    🐞 fix(layout): パンくずリストの不具合修正
    segmentの要素が2つ以上のときにsegmentの連結考慮ができておらず遷移に失敗していたので修正。また最後の現在のsegmentへのリンクである必要がないのでテキスト表示に修正

--- components/layout/breadcrumb-layout.tsx
+++ components/layout/breadcrumb-layout.tsx
@@ -15,28 +15,38 @@ import { usePathname, useSelectedLayoutSegments } from 'next/navigation'
 export const BreadcrumbLayout = () => {
   const segments = useSelectedLayoutSegments()
   const pathname = usePathname()
-
   const currentSegment = pathname.split('/').at(-1)
+
+  console.log({ segments })
 
   return (
     <Breadcrumb>
       <BreadcrumbList className="flex items-center pl-5 pt-8">
+        {/* ホームのリンクは常に表示する */}
         <BreadcrumbItem className={cn('' === currentSegment ? 'text-foreground' : '')}>
           <House size="1rem" />
           <BreadcrumbLink asChild>
             <Link href="/">Home</Link>
           </BreadcrumbLink>
         </BreadcrumbItem>
-        {segments.map((segment) => {
+        {segments.map((segment, index, arr) => {
+          if (arr.length === index + 1) {
+            // 最後の要素の場合はリンクではなくテキストを表示する
+            return (
+              <>
+                <BreadcrumbSeparator className="px-1" />
+                <p key={segment} className={segment === currentSegment ? 'text-foreground' : ''}>
+                  {segment}
+                </p>
+              </>
+            )
+          }
           return (
             <>
               <BreadcrumbSeparator className="px-1" />
-              <BreadcrumbItem
-                key={segment}
-                className={cn(segment === currentSegment ? 'text-foreground' : '')}
-              >
+              <BreadcrumbItem key={segment}>
                 <BreadcrumbLink asChild>
-                  <Link href={`/${segment}`}> {segment}</Link>
+                  <Link href={`/${arr.slice(0, index + 1).join('/')}`}> {segment}</Link>
                 </BreadcrumbLink>
               </BreadcrumbItem>
             </>
