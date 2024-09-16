ef867a5e61bdfbbd742e9738a5228030e2bdd970
hi-lee-mon <shunmel.09@gmail.com>
2024-09-16 12:22:41 +0900

    ✨ feat(middleware): 複数回実行されることを抑制
    middlewareを通過する必要のない処理はスキップするように設定

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,41 @@
 import { updateSession } from '@/lib/supabase/middleware'
-import { type NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  // ユーザのセッションを更新する
+  const { pathname } = request.nextUrl
+
+  // 画像ファイル系は処理をしない
+  if (
+    pathname.endsWith('.svg') ||
+    pathname.endsWith('.ico') ||
+    pathname.endsWith('.jpg') ||
+    pathname.endsWith('.jpeg') ||
+    pathname.endsWith('.png') ||
+    pathname.endsWith('.gif')
+  ) {
+    return NextResponse.next()
+  }
+
+  // リクエストがあったタイミングでユーザのセッションを更新する
   return await updateSession(request)
 }
 
 export const config = {
+  // middlewareを呼び出して良いパスを指定
   matcher: [
     /*
      * 以下のパスを除く全てのリクエストにマッチする
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+     * - その他、拡張子が *.(svg|png|jpg|jpeg|gif|webp) のファイル
+     */ {
+      source: '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+      // プリフェッチでmiddlewareが呼び出されないようにする
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
   ],
 }
